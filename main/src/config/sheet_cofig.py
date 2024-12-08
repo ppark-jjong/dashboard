8e8246a80caac45053b4eca7b551d33bf40e9f51
ppark__jjong <101304928+ppark-jjong@users.noreply.github.com>
2024-12-09 05:43:54 +0900

    no message

--- main/src/config/sheet_cofig.py
+++ main/src/config/sheet_cofig.py
@@ -1,15 +1,18 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv(dotenv_path="../.env")
+
 
 service_account_file = os.getenv("SERVICE_ACCOUNT_FILE")
 spreadsheet_id = os.getenv("SPREADSHEET_ID")
 api_key = os.getenv("API_KEY")
 
 class Config:
-    SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE", "path/to/service-account.json")
-    SPREADSHEET_ID = os.getenv("SPREADSHEET_ID", "your-spreadsheet-id")
-    DATA_FETCH_INTERVAL = int(os.getenv("DATA_FETCH_INTERVAL", 1800))  # Default: 30 minutes
+    SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE", "../../secrets/google_credentials.json")
+    SPREADSHEET_ID = os.getenv("SPREADSHEET_ID", "spreadsheet_id")
+    DATA_FETCH_INTERVAL = int(os.getenv("DATA_FETCH_INTERVAL", 1800))
     API_PORT = int(os.getenv("API_PORT", 5000))
     RANGE_NAME = os.getenv("RANGE_NAME", "Sheet1!A1:Z1000")
+
